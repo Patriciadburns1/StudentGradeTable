09a455adee4d03111842a5152668d18f98c8cdca
dburns1 <patriciadburns@gmail.com>
2018-07-30 16:24:11 -0700

    switched to react context api and also added routing installed axios and materialize

---
+++ src/components/studentlist.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import { Link } from 'react-router-dom'; 
+
+class StudentList extends Component{
+
+    render(){
+        return(
+            <div> 
+                <h1 className="center"> Adding Student List </h1> 
+            </div> 
+        )
+    }
+}
+
+export default StudentList; 
